5c8c58db646766447fab2fb5c2babc3818648ac2
emlycool <49098459+emlycool@users.noreply.github.com>
2019-09-20 22:10:04 +0100

    Update database.php

--- engine/database.php
+++ engine/database.php
@@ -1,5 +1,5 @@
 <?php 
-define('server', '	s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('server', '	localhost');
 define('username', 'h760c4fqmslytymg');
 define('password', 't8aa7eifrrc4xsin');
 define('database', 'y4cbjgvoxyr75u5h');
