69fe42bf238312980bfd46f6b5ba2a3233bf2cdb
emlycool <49098459+emlycool@users.noreply.github.com>
2019-09-20 17:54:00 +0100

    Update database.php

--- engine/database.php
+++ engine/database.php
@@ -1,8 +1,8 @@
 <?php 
-define('server', 'localhost');
-define('username', 'root');
-define('password', '');
-define('database', 'stage2');
+define('server', 'ec2-184-73-169-163.compute-1.amazonaws.com');
+define('username', 'yywbaokahafldj');
+define('password', 'b2f7d6723b6040aa120f12a2da3fb31f2563db54afc0dfe2034265ca0dcb8fb5');
+define('database', 'dahppq2r9t9fei');
 
 $mycon= mysqli_connect(server, username, password, database);
 
