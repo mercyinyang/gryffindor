7b5116793adb584c6ad98e7c736642cb1e645509
emlycool <49098459+emlycool@users.noreply.github.com>
2019-09-20 21:26:23 +0100

    jawsdb

--- engine/database.php
+++ engine/database.php
@@ -1,8 +1,8 @@
 <?php 
-define('server', 'ec2-184-73-169-163.compute-1.amazonaws.com');
-define('username', 'yywbaokahafldj');
-define('password', 'b2f7d6723b6040aa120f12a2da3fb31f2563db54afc0dfe2034265ca0dcb8fb5');
-define('database', 'dahppq2r9t9fei');
+define('server', '	s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('username', 'h760c4fqmslytymg');
+define('password', 't8aa7eifrrc4xsin');
+define('database', 'y4cbjgvoxyr75u5h');
 
 $mycon= mysqli_connect(server, username, password, database);
 
