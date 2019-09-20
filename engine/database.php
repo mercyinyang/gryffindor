db2d62d80ffaa758ff41f5c990ceef6da1d6518c
emlycool <49098459+emlycool@users.noreply.github.com>
2019-09-20 22:10:32 +0100

    Update database.php

--- engine/database.php
+++ engine/database.php
@@ -1,5 +1,5 @@
 <?php 
-define('server', '	localhost');
+define('server', 'localhost');
 define('username', 'h760c4fqmslytymg');
 define('password', 't8aa7eifrrc4xsin');
 define('database', 'y4cbjgvoxyr75u5h');
